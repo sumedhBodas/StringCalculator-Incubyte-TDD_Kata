40e6a43de82c03a1891893d0f1023e63d99f0abe
sumedhBodas <79521023+sumedhBodas@users.noreply.github.com>
2021-03-03 15:09:28 +0530

    Remaining test cases solution

--- StringCalculatorCode.java
+++ StringCalculatorCode.java
@@ -11,19 +11,22 @@ public class StringCalculatorCode {
 		{
 			CustomRegex.append(input, input.indexOf("//"), input.indexOf("\n"));
 			output.append(input.substring(input.indexOf("\n")).trim());
-		} else {
+		} 
+		else 
+		{
 			output.append(input);
 		}
- 
+ 				
 		return Arrays.stream(output.toString().split("[" + CustomRegex + "]"))
-				.map(String::strip)
-				.filter(j -> !j.isBlank())
-				.mapToInt(Integer::parseInt)
-				.filter(n -> n < 1001)
-				.peek(n -> {
-					if (n < 0) throw new IllegalArgumentException ("Negativesnot allowed: " + n);
-				})
-				.sum();
+			.map(String::strip)
+			.filter(j -> !j.isBlank())
+			.mapToInt(Integer::parseInt)
+			.filter(n -> n < 1001)
+			.peek(n -> 
+			{
+				if (n < 0) throw new IllegalArgumentException ("Negativesnot allowed: " + n);
+			}).sum();  
+				
 	}
 
 }
